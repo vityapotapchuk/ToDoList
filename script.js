7917ecb493a829423835bc52659d379a8d067413
vitosigm13611 <smplchnl@gmail.com>
2024-01-11 14:26:32 -0800

    add checked and deleted buttons

--- script.js
+++ script.js
@@ -11,16 +11,38 @@ btnNode.addEventListener('click', addToDo);
 //Добавим новый item в список 
 
 function addToDo(event) {
+
+    //Prevents form from relaoding;
+    event.preventDefault();
+
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
     // console.log(todoDiv);
     
     const newItem = document.createElement('li');
+    
 
     if (inputNode.value === '') {
         alert('You need write something!')
     } else {
-        alert('Good')
+        newItem.innerText = inputNode.value;
+        newItem.classList.add('todo-item');
+        todoDiv.appendChild(newItem);
+        toDoList.appendChild(todoDiv);
+        inputNode.value = '';
+
+        //Add checked button
+        const checked = document.createElement('button');
+        checked.innerHTML = '<i class="check"></i>';
+        checked.classList.add('checked-btn');
+        todoDiv.appendChild(checked);
+
+        //Add delete button
+        const deleted = document.createElement('button');
+        deleted.innerHTML = '<i class="trash"></i>';
+        deleted.classList.add('checked-btn');
+        todoDiv.appendChild(deleted);
+
     }
    
 
