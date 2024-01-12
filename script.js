5b5d155dfa0aff1ade7b8995bda9795842cdf93b
vitosigm13611 <smplchnl@gmail.com>
2024-01-11 20:04:58 -0800

    project done minimal function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const inputNode = document.querySelector('.js-input');
 const btnNode = document.querySelector('.js-btn');
-const toDoList = document.querySelector('.todoList');
+const todoList = document.querySelector('.todoList');
 
 //События
 
@@ -10,45 +10,32 @@ btnNode.addEventListener('click', addToDo);
 
 //Добавим новый item в список 
 
-function addToDo(event) {
-
-    //Prevents form from relaoding;
-    event.preventDefault();
-
+function addToDo (event) {
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
-    // console.log(todoDiv);
-    
+
     const newItem = document.createElement('li');
-    
 
     if (inputNode.value === '') {
-        alert('You need write something!')
+        alert("Write some text!")
     } else {
-        newItem.innerText = inputNode.value;
-        newItem.classList.add('todo-item');
+        newItem.classList.add('item');
         todoDiv.appendChild(newItem);
-        toDoList.appendChild(todoDiv);
+        newItem.innerText = inputNode.value;
+        todoDiv.addEventListener('click', () => {
+            newItem.style.textDecoration = 'line-through';
+        })
+        todoDiv.addEventListener('dblclick', () => {
+            todoList.removeChild(todoDiv);
+        })
+
+       
         inputNode.value = '';
-
-        //Add checked button
-        const checked = document.createElement('button');
-        checked.innerHTML = '<i class="check"></i>';
-        checked.classList.add('checked-btn');
-        todoDiv.appendChild(checked);
-
-        //Add delete button
-        const deleted = document.createElement('button');
-        deleted.innerHTML = '<i class="trash"></i>';
-        deleted.classList.add('checked-btn');
-        todoDiv.appendChild(deleted);
-
+        todoList.appendChild(todoDiv);
     }
-   
-
-    
-
-    
 
 }
+    
+
+
     
